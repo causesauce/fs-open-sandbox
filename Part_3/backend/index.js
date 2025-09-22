76c687a683d724a3fa884ba289d2ef8c3ce9c6a0
adrix <adrian.chervinchuk1@gmail.com>
2025-09-22 18:39:54 -0400

    - added prod build for frontend; - added static files referencing middleware to the backend that points to the frontend dist folder

--- Part_3/backend/index.js
+++ Part_3/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 const corsOptions = {
@@ -7,6 +8,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, './../../Part_2/dist')))
 
 let notes = [
     {
